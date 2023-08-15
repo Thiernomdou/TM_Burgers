0678eb64fe2a8f9a3239569ac5b3a544c6f0b8ed
DIALLO Thierno <thiernomdou63@gmail.com>
2023-08-15 18:18:22 +0200

    ✏️F01: LOGIN > Fix typo

--- src/components/Pages/login/LoginForm.jsx
+++ src/components/Pages/login/LoginForm.jsx
@@ -19,7 +19,7 @@ export default function LoginForm() {
   return (
     <div>
       <form action="submit" onSubmit={handleSubmit}>
-        <h1>Bienvenue chez nous</h1>
+        <h1>Bienvenue chez nous !</h1>
         <br />
         <h2>Connectez-vous</h2>
         <input value={inputValue} onChange={handleChange} type="text" placeholder="Entrez votre prénom..." required/>
