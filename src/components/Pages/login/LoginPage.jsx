a14bdc1803cde9fc3f87a02f397e80abe3346a54
DIALLO Thierno <thiernomdou63@gmail.com>
2023-09-21 20:51:21 +0200

    💄 F03 : LOGIN > Add styling and background

--- src/components/Pages/login/LoginPage.jsx
+++ src/components/Pages/login/LoginPage.jsx
@@ -3,7 +3,6 @@ import styled from "styled-components";
 import LoginForm from "./LoginForm";
 import Logo from "../../reusable-ui/Logo";
 
-
 export default function LoginPage() {
     //state
     
@@ -27,10 +26,26 @@ export default function LoginPage() {
 
 //styled components
 const LoginPageStyled = styled.div`
-  background: red;
   height: 100vh;
   display: flex;
   flex-direction: column;
   justify-content: center;
   align-items: center;
-`;
+
+
+
+  ::before {
+    content: "";
+    background: url("/images/burger-and-fries-background.jpg");
+    background-size: cover;
+    background-position: center;
+    background-blend-mode: darken;
+
+    position: absolute;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    z-index: -1;
+  }
+`
