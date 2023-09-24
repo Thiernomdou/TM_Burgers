1ce11b21bbd7395104dce3439f5a7fd265f5075a
DIALLO Thierno <thiernomdou63@gmail.com>
2023-09-25 00:01:37 +0200

    💄 F03 : LOGIN > loginForm > button > Add styling

--- src/components/reusable-ui/Logo.jsx
+++ src/components/reusable-ui/Logo.jsx
@@ -4,7 +4,7 @@ import styled from "styled-components";
 export default function Logo() {
   return (
     <LogoStyled>
-      <h1>CRAZEE BURGER</h1>
+      <h1>CRAZEE</h1>
       <img src="/images/logo-orange.png" alt="logo-crazee-burger" />
       <h1>BURGER</h1>
     </LogoStyled>
