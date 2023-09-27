27825adec354655172c6fe82f19be3826f1e4ced
DIALLO Thierno <thiernomdou63@gmail.com>
2023-09-27 23:56:46 +0200

    🚧 F04 : ORDER > layout > add navbar and main

--- src/components/Pages/order/OrderPage.jsx
+++ src/components/Pages/order/OrderPage.jsx
@@ -12,10 +12,16 @@ export default function OrderPage() {
   return (
     <OrderPageStyled>
       <div className="container">
-        <h1>Bonjour {username}</h1>
-        <Link to="/">
-          <button>Déconnexion</button>
-        </Link>
+        <div className="navbar">
+          Navbar
+          <h1>Bonjour {username}</h1>
+          <Link to="/">
+            <button>Déconnexion</button>
+          </Link>
+        </div>
+        <div className="main">
+            Main
+        </div>
       </div>
     </OrderPageStyled>
   )
@@ -33,5 +39,16 @@ const OrderPageStyled = styled.div`
     background: red;
     height: 95vh;
     width: 1400px;
+    display: flex;
+    flex-direction: column;
+  }
+
+  .navbar{
+    background: blue;
+    height: 10vh;
+  }
+  .main{
+    background: green;
+    flex: 1;
   }
 `;
