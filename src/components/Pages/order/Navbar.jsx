abd7d24646a2ff66b689f163d710d6685de7a069
DIALLO Thierno <thiernomdou63@gmail.com>
2023-09-28 16:04:30 +0200

    ✨ F04 : ORDER > Navbar > Add Logo

--- src/components/Pages/order/Navbar.jsx
+++ src/components/Pages/order/Navbar.jsx
@@ -1,6 +1,6 @@
-
 import styled from 'styled-components';
 import NavbarRightSide from "./NavbarRightSide"
+import Logo from "../../reusable-ui/Logo"
 
 export default function Navbar({username}) {
     //state
@@ -10,9 +10,7 @@ export default function Navbar({username}) {
     //affichage(render)
   return (
     <NavbarStyled>
-      <div className="left-side">
-        Left
-      </div>
+      <Logo />
       <NavbarRightSide username={username}/>
     </NavbarStyled>
   )
@@ -24,9 +22,6 @@ const NavbarStyled = styled.nav`
     height: 10vh;
     display: flex;
     justify-content: space-between;
+    padding: 0 20px;
     /* align-items: center; */
-
-    .left-side{
-      background: pink;
-    }
 `;
