3bdc87275c7c21b6200ccbfeb443e7a93af3280b
DIALLO Thierno <thiernomdou63@gmail.com>
2023-10-07 00:06:32 +0200

    💄 F05 : ORDER > MENU > Add final Style

--- src/components/Pages/order/Main/Menu.jsx
+++ src/components/Pages/order/Main/Menu.jsx
@@ -1,6 +1,7 @@
 import styled from "styled-components";
 import {fakeMenu2} from "../../../../fakeData/fakeMenu"
 import { useState } from "react";
+import {theme} from "../../../../theme"
 
 export default function Menu() {
     //state
@@ -23,12 +24,13 @@ export default function Menu() {
 
 //stylecomponents
 const MenuStyled = styled.div`
-      background: purple;
+      background: ${theme.colors.background_white};
       display: grid;
       grid-template-columns: repeat(4, 1fr);
       grid-row-gap: 60px;
       padding: 50px 50px 150px;
       justify-items: center;
+      box-shadow: 0px 8px 20px 8px rgba(0,0,0,0.2) inset;
 
       .produit {
         background: red;
