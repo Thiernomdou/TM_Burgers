2be9613fe50c6a3620fda4636cf2f590b75be895
DIALLO Thierno <thiernomdou63@gmail.com>
2023-10-08 12:56:44 +0200

    ♻️ F05 : ORDER > MENU > Create Product

---
+++ src/components/Pages/order/Main/Product.jsx
@@ -0,0 +1,45 @@
+import React from 'react'
+import styled from 'styled-components';
+
+export default function Product({imageSource, title, price }) {
+  //state
+
+  //comportements
+
+  //afficher(render)
+  return (
+    <ProductStyled className="produit">
+        <div className="image">
+            <img src={imageSource} alt={title} />
+        </div>
+        <div className="info-text">
+            <div className="description">
+            <div className="title">{title}</div>
+            <div className="price">{price}</div>
+            <button className="add-button">Ajouter</button>
+            </div>
+        </div>
+    </ProductStyled>
+  )
+}
+
+//styledcomponents
+const ProductStyled = styled.div`
+  background: red;
+    width: 240px;
+    height: 330px;
+
+  .image {
+    border: 1px solid fuchsia;
+    width: 100px;
+    height: auto;
+    img {
+      width: 100%;
+      height: 100%;
+    }
+  }
+
+  .description {
+    border: 1px solid fuchsia;
+  }
+`;
